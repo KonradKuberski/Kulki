cdc26428626958f9180c65866799fea14de2af50
251532 <Olciak5920048753@>
2025-04-10 17:54:48 +0200

    ball colisions with walls and each other

--- ReactiveInteractiveUserInterface/Data/Ball.cs
+++ ReactiveInteractiveUserInterface/Data/Ball.cs
@@ -10,41 +10,74 @@
 
 namespace TP.ConcurrentProgramming.Data
 {
-  internal class Ball : IBall
-  {
-    #region ctor
-
-    internal Ball(Vector initialPosition, Vector initialVelocity)
+    internal class Ball : IBall
     {
-      Position = initialPosition;
-      Velocity = initialVelocity;
+        #region ctor
+        internal Ball(Vector initialPosition, Vector initialVelocity)
+        {
+            PositionBackingField = initialPosition;
+            Velocity = initialVelocity;
+            Radius = 10.0; // Promień = 10, bo srednica to 20
+        }
+        #endregion ctor
+
+        #region IBall
+        public event EventHandler<IVector>? NewPositionNotification;
+        public IVector Velocity { get; set; }
+        public IVector Position => PositionBackingField; 
+        public double Radius { get; } 
+        #endregion IBall
+
+        #region private
+        private Vector PositionBackingField;
+
+        private void RaiseNewPositionChangeNotification()
+        {
+            NewPositionNotification?.Invoke(this, PositionBackingField);
+        }
+
+        internal void Move(Vector delta)
+        {
+            double scale = 0.016;
+            double newX = PositionBackingField.x + delta.x * scale;
+            double newY = PositionBackingField.y + delta.y * scale;
+            double diameter = 2 * Radius;
+            double maxX = 396.0 - diameter; 
+            double maxY = 416.0 - diameter;
+            Vector newVelocity = (Vector)Velocity;
+
+            if (newX < 0)
+            {
+                newX = 0;
+                newVelocity = new Vector(-newVelocity.x, newVelocity.y);
+            }
+            else if (newX > maxX)
+            {
+                newX = maxX;
+                newVelocity = new Vector(-newVelocity.x, newVelocity.y);
+            }
+
+            if (newY < 0)
+            {
+                newY = 0;
+                newVelocity = new Vector(newVelocity.x, -newVelocity.y);
+            }
+            else if (newY > maxY)
+            {
+                newY = maxY;
+                newVelocity = new Vector(newVelocity.x, -newVelocity.y);
+            }
+
+            PositionBackingField = new Vector(newX, newY);
+            Velocity = newVelocity;
+            RaiseNewPositionChangeNotification();
+        }
+
+        internal void UpdatePosition(Vector newPosition)
+        {
+            PositionBackingField = newPosition;
+            RaiseNewPositionChangeNotification();
+        }
+        #endregion private
     }
-
-    #endregion ctor
-
-    #region IBall
-
-    public event EventHandler<IVector>? NewPositionNotification;
-
-    public IVector Velocity { get; set; }
-
-    #endregion IBall
-
-    #region private
-
-    private Vector Position;
-
-    private void RaiseNewPositionChangeNotification()
-    {
-      NewPositionNotification?.Invoke(this, Position);
-    }
-
-    internal void Move(Vector delta)
-    {
-      Position = new Vector(Position.x + delta.x, Position.y + delta.y);
-      RaiseNewPositionChangeNotification();
-    }
-
-    #endregion private
-  }
 }
--- ReactiveInteractiveUserInterface/Data/DataImplementation.cs
+++ ReactiveInteractiveUserInterface/Data/DataImplementation.cs
@@ -13,100 +13,172 @@ using System.Diagnostics;
 
 namespace TP.ConcurrentProgramming.Data
 {
-  internal class DataImplementation : DataAbstractAPI
-  {
-    #region ctor
-
-    public DataImplementation()
+    internal class DataImplementation : DataAbstractAPI
     {
-      MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
-    }
-
-    #endregion ctor
-
-    #region DataAbstractAPI
-
-    public override void Start(int numberOfBalls, Action<IVector, IBall> upperLayerHandler)
-    {
-      if (Disposed)
-        throw new ObjectDisposedException(nameof(DataImplementation));
-      if (upperLayerHandler == null)
-        throw new ArgumentNullException(nameof(upperLayerHandler));
-      Random random = new Random();
-      for (int i = 0; i < numberOfBalls; i++)
-      {
-        Vector startingPosition = new(random.Next(100, 400 - 100), random.Next(100, 400 - 100));
-        Ball newBall = new(startingPosition, startingPosition);
-        upperLayerHandler(startingPosition, newBall);
-        BallsList.Add(newBall);
-      }
-    }
-
-    #endregion DataAbstractAPI
-
-    #region IDisposable
-
-    protected virtual void Dispose(bool disposing)
-    {
-      if (!Disposed)
-      {
-        if (disposing)
+        #region ctor
+        public DataImplementation()
         {
-          MoveTimer.Dispose();
-          BallsList.Clear();
+            MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(1));
         }
-        Disposed = true;
-      }
-      else
-        throw new ObjectDisposedException(nameof(DataImplementation));
+        #endregion ctor
+
+        #region DataAbstractAPI
+        public override void Start(int numberOfBalls, Action<IVector, IBall> upperLayerHandler)
+        {
+            if (Disposed)
+                throw new ObjectDisposedException(nameof(DataImplementation));
+            if (upperLayerHandler == null)
+                throw new ArgumentNullException(nameof(upperLayerHandler));
+            Random random = new Random();
+
+            // Lista wszystkich możliwych kombinacji prędkości (do przemyślenia)
+            Vector[] possibleVelocities = new Vector[]
+            {
+             new Vector(15.0, 7.5),    
+             new Vector(-15.0, -7.5), 
+             new Vector(15.0, -7.5),   
+             new Vector(-15.0, 7.5),  
+             new Vector(7.5, 15.0),    
+             new Vector(-7.5, -15.0),  
+             new Vector(7.5, -15.0),  
+             new Vector(-7.5, 15.0)    
+            };
+
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                Vector startingPosition = new(random.Next(100, 300), random.Next(100, 320)); 
+                Vector initialVelocity = possibleVelocities[random.Next(possibleVelocities.Length)];
+                Ball newBall = new(startingPosition, initialVelocity);
+                upperLayerHandler(startingPosition, newBall);
+                BallsList.Add(newBall);
+            }
+        }
+        #endregion DataAbstractAPI
+
+        #region IDisposable
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    MoveTimer.Dispose();
+                    BallsList.Clear();
+                }
+                Disposed = true;
+            }
+            else
+                throw new ObjectDisposedException(nameof(DataImplementation));
+        }
+
+        public override void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion IDisposable
+
+        #region private
+        private bool Disposed = false;
+        private readonly Timer MoveTimer;
+        private Random RandomGenerator = new();
+        private List<Ball> BallsList = [];
+
+        private void Move(object? x)
+        {
+            HandleCollisions();
+
+            foreach (Ball item in BallsList)
+            {
+                item.Move((Vector)item.Velocity);
+            }
+        }
+
+        private void HandleCollisions()
+        {
+            for (int i = 0; i < BallsList.Count; i++)
+            {
+                for (int j = i + 1; j < BallsList.Count; j++)
+                {
+                    Ball ball1 = BallsList[i] as Ball;
+                    Ball ball2 = BallsList[j] as Ball;
+
+                    if (ball1 == null || ball2 == null) continue;
+
+                    double dx = ball2.Position.x - ball1.Position.x;
+                    double dy = ball2.Position.y - ball1.Position.y;
+                    double distance = Math.Sqrt(dx * dx + dy * dy);
+
+                    // Sprawdzamy, czy kulki się stykają (odległość mniejsza lub równa sumie promieni)
+                    if (distance <= (ball1.Radius + ball2.Radius))
+                    {
+                        if (distance == 0) // Unikamy dzielenia przez zero
+                        {
+                            dx = 1.0;
+                            dy = 0.0;
+                            distance = 1.0;
+                        }
+                        double nx = dx / distance;
+                        double ny = dy / distance;
+
+                        // Obliczamy względną prędkość
+                        double dvx = ball2.Velocity.x - ball1.Velocity.x;
+                        double dvy = ball2.Velocity.y - ball1.Velocity.y;
+
+                        // Obliczamy składową prędkości wzdłuż wektora kolizji
+                        double dotProduct = dvx * nx + dvy * ny;
+
+                        // Jeśli kulki zbliżają się do siebie (dotProduct < 0), obliczamy nowe prędkości
+                        if (dotProduct < 0)
+                        {
+                            // Zakładamy, że kulki mają tę samą masę (sprężyste zderzenie)
+                            double impulse = 2 * dotProduct / 2.0; // Dzielimy przez 2, bo masa = 1 dla obu kulek
+
+                            // Aktualizujemy prędkości
+                            ball1.Velocity = new Vector(
+                              ball1.Velocity.x + impulse * nx,
+                              ball1.Velocity.y + impulse * ny
+                            );
+                            ball2.Velocity = new Vector(
+                              ball2.Velocity.x - impulse * nx,
+                              ball2.Velocity.y - impulse * ny
+                            );
+
+                            // Rozdzielamy kulki, aby się nie skleiły
+                            double overlap = (ball1.Radius + ball2.Radius - distance) / 2;
+                            ball1.UpdatePosition(new Vector(
+                              ball1.Position.x - overlap * nx,
+                              ball1.Position.y - overlap * ny
+                            ));
+                            ball2.UpdatePosition(new Vector(
+                              ball2.Position.x + overlap * nx,
+                              ball2.Position.y + overlap * ny
+                            ));
+                        }
+                    }
+                }
+            }
+        }
+        #endregion private
+
+        #region TestingInfrastructure
+        [Conditional("DEBUG")]
+        internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
+        {
+            returnBallsList(BallsList);
+        }
+
+        [Conditional("DEBUG")]
+        internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
+        {
+            returnNumberOfBalls(BallsList.Count);
+        }
+
+        [Conditional("DEBUG")]
+        internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
+        {
+            returnInstanceDisposed(Disposed);
+        }
+        #endregion TestingInfrastructure
     }
-
-    public override void Dispose()
-    {
-      // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-      Dispose(disposing: true);
-      GC.SuppressFinalize(this);
-    }
-
-    #endregion IDisposable
-
-    #region private
-
-    //private bool disposedValue;
-    private bool Disposed = false;
-
-    private readonly Timer MoveTimer;
-    private Random RandomGenerator = new();
-    private List<Ball> BallsList = [];
-
-    private void Move(object? x)
-    {
-      foreach (Ball item in BallsList)
-        item.Move(new Vector((RandomGenerator.NextDouble() - 0.5) * 10, (RandomGenerator.NextDouble() - 0.5) * 10));
-    }
-
-    #endregion private
-
-    #region TestingInfrastructure
-
-    [Conditional("DEBUG")]
-    internal void CheckBallsList(Action<IEnumerable<IBall>> returnBallsList)
-    {
-      returnBallsList(BallsList);
-    }
-
-    [Conditional("DEBUG")]
-    internal void CheckNumberOfBalls(Action<int> returnNumberOfBalls)
-    {
-      returnNumberOfBalls(BallsList.Count);
-    }
-
-    [Conditional("DEBUG")]
-    internal void CheckObjectDisposed(Action<bool> returnInstanceDisposed)
-    {
-      returnInstanceDisposed(Disposed);
-    }
-
-    #endregion TestingInfrastructure
-  }
 }
